f60bd805616089929165c9b00ce7d835ee49babb
Frederik S. Held <git@frederikheld.de>
2022-03-25 02:22:15 +0100

    Basic Python GPIO test

--- hvac.py
+++ hvac.py
@@ -1,1 +1,25 @@
+#!/usr/bin/python3
+
+import time
+import RPi.GPIO as GPIO
+import Adafruit_DHT as DHT
+
 print('Hello HVAC!')
+
+GPIO.setmode(GPIO.BOARD)
+
+# configure pins:
+pin_fan_out = 4
+pin_dht22_in = 23
+
+# set pin modes:
+GPIO.setup(pin_fan_out, GPIO.OUT) # fan out
+
+while True:
+    current_humidity, current_temperature = DHT.read_entry(DHT.DHT22, pin_dht22_in)
+
+    print('Temperature: {0:0.1f} °C, Humidity: {1:0.1f} %'. format(current_temperature, current_humidity))
+    
+    time.sleep(1) # sleep one second
+
+GPIO.cleanup()
